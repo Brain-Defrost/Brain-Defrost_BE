9bade7d4b655bb0a1e73f9f4fce4197f87eba9c5
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-11 14:10:40 -0500

    Update stat.rb

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,7 +1,6 @@
 class Stat < ApplicationRecord
   belongs_to :game
 
-
   validates :game_id, presence: true
   validates :avg_correct_answers, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
