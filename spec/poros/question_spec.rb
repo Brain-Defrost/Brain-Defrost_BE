ad168055936c0db02a1e7dcbd222ecf9f023c1a7
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-12 14:27:43 -0600

    Fix Question poro tests to include change in prompt requesting topic, and agreed format in mock server

--- spec/poros/question_spec.rb
+++ spec/poros/question_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Question do
   before do
-    data = {:id=>1, :question=>"Who is known as the Queen of Pop?", :correct_answer=>"Madonna", :options=>["Madonna", "Beyonce", "Taylor Swift", "Adele"]}
+    data = {"id"=>8,
+    "topic"=>"Music Trivia",
+    "question_text"=>"What is the name of Rihanna's debut single?",
+    "correct_answer"=>"Pon de Replay",
+    "options"=>["Umbrella", "Diamonds", "Love on the Brain", "Pon de Replay"]}
 
     @question = Question.new(data)
   end
@@ -10,10 +14,11 @@ RSpec.describe Question do
   describe '#initialize' do
     it 'exists and populates attributes correctly' do
       expect(@question).to be_a(Question)
-      expect(@question.id).to eq(1)
-      expect(@question.question).to eq("Who is known as the Queen of Pop?")
-      expect(@question.correct_answer).to eq("Madonna")
-      expect(@question.options).to eq(["Madonna", "Beyonce", "Taylor Swift", "Adele"])
+      expect(@question.question_number).to eq(8)
+      expect(@question.topic).to eq("Music Trivia")
+      expect(@question.question_text).to eq("What is the name of Rihanna's debut single?")
+      expect(@question.answer).to eq("Pon de Replay")
+      expect(@question.options).to eq(["Umbrella", "Diamonds", "Love on the Brain", "Pon de Replay"])
     end
   end
 end
