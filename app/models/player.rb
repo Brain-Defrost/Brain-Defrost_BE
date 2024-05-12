ea614e8e2109f06c131ef623d551ce153a1d8787
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-11 21:24:42 -0500

    update models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :game
-  
-  validates :display_name, presence: true, length: { minimum: 1, maximum: 30 }, uniqueness: { scope: :game_id }
+
+  validates :display_name, presence: true, length: { within: 1..30 }, uniqueness: { scope: :game_id }
   validates_presence_of :display_name
   validates_uniqueness_of :display_name, scope: :game_id
   validates :answers_correct, presence: true, numericality: { greater_than_or_equal_to: 0 }
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  describe "validations" do
+    let!(:player) { create(:player) }
 
-  it { should validate_presence_of(:display_name) }
-  it { should validate_length_of(:display_name).is_at_least(1).is_at_most(30) }
-  it { should validate_presence_of(:answers_correct) }
-  it { should validate_numericality_of(:answers_correct).is_greater_than_or_equal_to(0) }
-  it { should validate_presence_of(:answers_incorrect) }
-  it { should validate_numericality_of(:answers_incorrect).is_greater_than_or_equal_to(0) }
-  it { should validate_presence_of(:game_id) }
+    it { should validate_presence_of(:display_name) }
+    it { should validate_length_of(:display_name).is_at_least(1).is_at_most(30) }
+    it { should validate_uniqueness_of(:display_name).scoped_to(:game_id) }
+    it { should validate_presence_of(:answers_correct) }
+    it { should validate_numericality_of(:answers_correct).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of(:answers_incorrect) }
+    it { should validate_numericality_of(:answers_incorrect).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of(:game_id) }
+  end
 
-  # Association test
-  it { should belong_to(:game) }
+  describe "associations" do
+    it { should belong_to(:game) }
+  end
 end
