9a4dfabe664baa656faa7b0754b8c4e64e7a8300
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-14 14:56:09 -0600

    Comment relations line

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -2,5 +2,5 @@ class GameSerializer
   include JSONAPI::Serializer
   attributes :link, :started, :number_of_questions, :number_of_players, :topic, :time_limit
 
-  has_many :players, :questions
+  # has_many :players, :questions
 end
