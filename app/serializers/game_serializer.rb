6af6b188f8408a47a95ed37458963d09fe50c7a2
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-14 14:35:20 -0600

    Feat: add relations to game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,6 @@
 class GameSerializer
   include JSONAPI::Serializer
   attributes :link, :started, :number_of_questions, :number_of_players, :topic, :time_limit
+
+  has_many :players, :questions
 end
