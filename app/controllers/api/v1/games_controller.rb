f9a2a6fa181c733add0cdfbee13e60062b7c2e8a
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-14 21:17:09 -0500

    game API and serializer

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,13 +1,30 @@
 class Api::V1::GamesController < ApplicationController
   def create
-    #code
+    game = Game.new(new_game_params)
+    if game.save!
+      game.players.create!(display_name: params[:display_name])
+      questions = QuestionServiceFacade.get_questions(game)
+      render json: GameSerializer.new(game, questions), status: :created
+    end
   end
 
   def show
-    #code
+    game = Game.find(params[:id])
+    render json: GameSerializer.new(game)
   end
 
   def update
-    #code
+    game = Game.find(params[:id])
+    game.update!(started: params[:started])
+    render json: GameSerializer.new(game)
+  end
+
+  private
+  def new_game_params
+    params.permit(:topic, :number_of_questions, :number_of_players, :time_limit, :link) # remove link later
+  end
+
+  def game_params
+    params.permit(:started)
   end
 end
--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -1,71 +1,250 @@
-# require 'swagger_helper'
+require 'swagger_helper'
+require 'securerandom'
 
-# RSpec.describe 'api/v1/games', type: :request do
+RSpec.describe 'Game API', type: :request do
 
-#   path '/api/v1/games' do
+    path '/api/v1/games' do
 
-#     post('create game') do
-#       response(200, 'successful') do
+      post('Create a new trivia game') do
+        tags 'Game'
+        consumes 'application/json'
+        produces 'application/json'
 
-#         after do |example|
-#           example.metadata[:response][:content] = {
-#             'application/json' => {
-#               example: JSON.parse(response.body, symbolize_names: true)
-#             }
-#           }
-#         end
-#         run_test!
-#       end
-#     end
-#   end
+        parameter name: :params, in: :body, required: true, schema: {
+          type: :object,
+          properties: {
+            topic: { type: :string, required: true },
+            number_of_questions: { type: :integer, required: true },
+            time_limit: { type: :integer, required: true },
+            number_of_players: { type: :integer, required: true },
+            display_name: { type: :string, required: true },
+            link: { type: :string } # remove later
+          }
+        }
 
-#   path '/api/v1/games/{id}' do
-#     # You'll want to customize the parameter types...
-#     parameter name: 'id', in: :path, type: :string, description: 'id'
+        response(201, 'successful') do
+          let(:params) { {
+            topic: "music",
+            number_of_questions: 8,
+            time_limit: 30,
+            number_of_players: 7,
+            display_name: "trivia_genius",
+            link: "www.example.com/#{SecureRandom.hex(5)}" # remove later
+          } }
 
-#     get('show game') do
-#       response(200, 'successful') do
-#         let(:id) { '123' }
+          schema({
+            type: :object,
+            properties: {
+              data: {
+                type: :object,
+                properties: {
+                  id: { type: :string },
+                  type: { type: :string },
+                  attributes: {
+                    type: :object,
+                    properties: {
+                      link: { type: :string, required: true },
+                      started: { type: :boolean },
+                      number_of_questions: { type: :integer },
+                      number_of_players: { type: :integer },
+                      topic: { type: :string },
+                      time_limit: { type: :integer }
+                    }
+                  },
+                  relationships: {
+                    type: :object,
+                    properties: {
+                      players: {
+                        type: :object,
+                        properties: {
+                          data: {
+                            type: :array,
+                            items: {
+                              type: :object,
+                              properties: {
+                                id: { type: :string },
+                                type: { type: :string },
+                                attributes: {
+                                  type: :object,
+                                  properties: {
+                                    display_name: { type: :string },
+                                    answers_correct: { type: :integer },
+                                    answers_incorrect: { type: :integer }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      },
+                      questions: {
+                        type: :object,
+                        properties: {
+                          data: {
+                            type: :array,
+                            items: {
+                              type: :object,
+                              properties: {
+                                id: { type: [:string, :null] },
+                                type: { type: :string },
+                                attributes: {
+                                  type: :object,
+                                  properties: {
+                                    topic: { type: :string },
+                                    question_text: { type: :string },
+                                    question_number: { type: :integer },
+                                    answer: { type: :string },
+                                    options: {
+                                      type: :array,
+                                      items: { type: :string }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          })
 
-#         after do |example|
-#           example.metadata[:response][:content] = {
-#             'application/json' => {
-#               example: JSON.parse(response.body, symbolize_names: true)
-#             }
-#           }
-#         end
-#         run_test!
-#       end
-#     end
+          run_test! do |example|
+            expect(response.status).to eq 201
+            parsed_data = JSON.parse(response.body, symbolize_names: true)[:data]
+            expect(parsed_data[:id].to_i).to eq Game.last.id
+            expect(parsed_data[:type]).to eq "game"
+            expect(parsed_data[:attributes][:display_name]).to eq "trivia-ninja"
+            expect(parsed_data[:attributes][:answers_correct]).to eq 0
+            expect(parsed_data[:attributes][:answers_incorrect]).to eq 0
+            expect(response.body)
+          end
+        end
+      end
+    end
 
-#     patch('update game') do
-#       response(200, 'successful') do
-#         let(:id) { '123' }
+    path '/api/v1/games/{id}' do
+      parameter name: 'id', in: :path, type: :string, description: 'Game ID'
 
-#         after do |example|
-#           example.metadata[:response][:content] = {
-#             'application/json' => {
-#               example: JSON.parse(response.body, symbolize_names: true)
-#             }
-#           }
-#         end
-#         run_test!
-#       end
-#     end
+      get('show game') do
+        # parameter name: 'id', in: :path, type: :integer, description: 'Game ID'
+        tags 'Game'
+        produces 'application/json'
 
-#     put('update game') do
-#       response(200, 'successful') do
-#         let(:id) { '123' }
+        response(200, 'successful') do
+          schema({
+            type: :object,
+            properties: {
+              data: {
+                type: :object,
+                properties: {
+                  id: { type: :string },
+                  type: { type: :string },
+                  attributes: {
+                    type: :object,
+                    properties: {
+                      link: { type: :string },
+                      started: { type: :boolean },
+                      number_of_questions: { type: :integer },
+                      number_of_players: { type: :integer },
+                      topic: { type: :string },
+                      time_limit: { type: :integer }
+                    }
+                  },
+                  relationships: {
+                    type: :object,
+                    properties: {
+                      players: {
+                        type: :object,
+                        properties: {
+                          data: {
+                            type: :array,
+                            items: {
+                              type: :object,
+                              properties: {
+                                id: { type: :string },
+                                type: { type: :string },
+                                attributes: {
+                                  type: :object,
+                                  properties: {
+                                    display_name: { type: :string },
+                                    answers_correct: { type: :integer },
+                                    answers_incorrect: { type: :integer }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          })
 
-#         after do |example|
-#           example.metadata[:response][:content] = {
-#             'application/json' => {
-#               example: JSON.parse(response.body, symbolize_names: true)
-#             }
-#           }
-#         end
-#         run_test!
-#       end
-#     end
-#   end
-# end
+          let(:game_1) { create(:game) }
+          let(:player_1) { create(:player, game: game_1)}
+          let(:player_2) { create(:player, game: game_1)}
+          let(:id) { game_1.id }
+
+          run_test! do |response|
+            expect(response).to have_http_status(200)
+          end
+        end
+
+      patch('update game') do
+        tags 'Game'
+        consumes 'application/json'
+        produces 'application/json'
+
+        parameter name: :params, in: :body, schema: {
+          type: :object,
+          properties: {
+            type: :boolean, required: true
+          }
+        }
+
+        response(200, 'successful') do
+          let(:id) { create(:game, started: false).id }
+          let(:params) { { started: true } }
+
+          schema({ 
+            type: :object,
+            properties: {
+              data: {
+                type: :object,
+                properties: {
+                  id: { type: :string },
+                  type: { type: :string },
+                  attributes: {
+                    type: :object,
+                    properties: {
+                      link: { type: :string },
+                      started: { type: :boolean },
+                      number_of_questions: { type: :integer },
+                      number_of_players: { type: :integer },
+                      topic: { type: :string },
+                      time_limit: { type: :integer }
+                    }
+                  }
+                }
+              }
+            }
+          })
+
+          run_test! do |response|
+            expect(response.status).to eq 200
+            parsed_data = JSON.parse(response.body, symbolize_names: true)[:data]
+            expect(parsed_data[:id].to_i).to eq(id)
+            expect(parsed_data[:attributes][:started]).to eq true
+          end
+        end
+      end
+    end
+  end
+end
