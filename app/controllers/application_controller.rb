2adbb0aea181629228f6484a159ee961440205f3
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-14 21:11:07 -0500

    error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :error_message
+  rescue_from ActiveRecord::RecordInvalid, with: :error_message
+
+  def error_message(exception)
+    if exception.message.include?("exist") || exception.message.include?("find")
+      render json: { error: { message: exception.message } }, status: :not_found
+    else
+      render json: { error: { message: exception.message } }, status: :bad_request
+    end
+  end
 end
