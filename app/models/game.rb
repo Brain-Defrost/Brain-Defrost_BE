fe60912bff5ed8a8eb069420135f6238feae9cef
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-15 18:44:15 -0600

    Feat: Create method for link creation

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,6 +5,9 @@ class Game < ApplicationRecord
   validates :number_of_questions, presence: true, numericality: { in: 1..10 }
   validates :time_limit, presence: true, numericality: { in: 5..120 }
   validates :number_of_players, presence: true, numericality: { in: 1..35 }
-  validates :link, presence: true, uniqueness: true
   validates :started, inclusion: [true, false]
+
+  after_save do
+    self.link = "https://brain-defrost.github.io/Brain-Defrost_FE/join/#{self.id}"
+  end
 end
