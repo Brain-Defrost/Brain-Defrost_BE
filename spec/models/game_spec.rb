574c39425a274c82025c17ecb18c6ae9c03ce8fa
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-15 18:43:46 -0600

    Test: Create tests for link creation

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -10,8 +10,6 @@ RSpec.describe Game, type: :model do
     it { should validate_numericality_of(:time_limit).is_in(5..120) }
     it { should validate_presence_of(:number_of_players) }
     it { should validate_numericality_of(:number_of_players).is_in(1..35) }
-    it { should validate_presence_of(:link) }
-    it { should validate_uniqueness_of(:link) }
 
     it "validates 'started' is a boolean" do
       game_1 = create(:game, started: true)
@@ -23,5 +21,20 @@ RSpec.describe Game, type: :model do
 
   describe "associations" do
     it { should have_many(:players).dependent(:destroy) }
+  end
+  
+  describe "after_save block" do
+      it "generates unique links upon creation" do
+        game = Game.new({number_of_questions: 8, number_of_players: 7, topic: "music", time_limit: 30})
+  
+        game.save
+        expect(game.link).to_not be_nil
+      end
+  
+      it "generates a valid link with correct format" do
+        game = Game.create!({number_of_questions: 8, number_of_players: 7, topic: "music", time_limit: 30})
+  
+        expect(game.link).to match("https://brain-defrost.github.io/Brain-Defrost_FE/join/#{game.id}")
+      end
   end
 end
