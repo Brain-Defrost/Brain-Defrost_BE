d1ce0268b5d11cbb4461e373bbddb1c472d1a5b2
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-18 18:21:04 -0500

    update question poro
    Co-authored-by: Laura R Vega <149700247+laurarvegav@users.noreply.github.com>

--- spec/poros/question_spec.rb
+++ spec/poros/question_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Question do
-
   before do
     data = {
       id: nil, 
@@ -20,13 +19,12 @@ RSpec.describe Question do
 
   describe '#initialize' do
     it 'exists and populates attributes correctly' do
-      require 'pry'; binding.pry
       expect(@question).to be_a(Question)
       expect(@question.question_number).to eq("1")
       expect(@question.question_text).to eq("Who is the Doctor's main nemesis?")
       expect(@question.answer).to eq("The Master")
       expect(@question.options).to be_an(Array)
-      expect(@questions.options).to include("Cybermen", "The Master", "Daleks", "Silence")
+      expect(@question.options).to include("Cybermen", "The Master", "Daleks", "Silence")
     end
   end
 end
