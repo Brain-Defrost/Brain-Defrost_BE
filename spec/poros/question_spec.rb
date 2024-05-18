907ea2ddbae84d4de828c3f8442deb6101e3729c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-05-17 18:03:34 -0600

    Create tests for new method that assures options array contains 4 elements

--- spec/poros/question_spec.rb
+++ spec/poros/question_spec.rb
@@ -7,8 +7,22 @@ RSpec.describe Question do
     "question_text"=>"What is the name of Rihanna's debut single?",
     "correct_answer"=>"Pon de Replay",
     "options"=>["Umbrella", "Diamonds", "Love on the Brain", "Pon de Replay"]}
+    
+    data_2 = {"id"=>9,
+    "topic"=>"Chess Trivia",
+    "question_text"=>"What is the most powerful piece in chess?",
+    "correct_answer"=>"Queen",
+    "options"=>["Queen", "King", "Knight", "Pawn"]}
+  
+    data_incomplete = {"id"=>10,
+    "topic"=>"Music Trivia",
+    "question_text"=>"What is the name of Rihanna's debut single?",
+    "correct_answer"=>"Pon de Replay",
+    "options"=>["Umbrella", "Diamonds", "Love on the Brain"] }
 
     @question = Question.new(data)
+    @question_2 = Question.new(data_2)
+    @question_3 = Question.new(data_incomplete)
   end
 
   describe '#initialize' do
@@ -19,22 +33,21 @@ RSpec.describe Question do
       expect(@question.question_text).to eq("What is the name of Rihanna's debut single?")
       expect(@question.answer).to eq("Pon de Replay")
       expect(@question.options).to be_an(Array)
-      expect(@question.options.length).to eq(4)
-      expect(@question.options.include?("Pon de Replay")).to eq(true)
     end
+  end
 
-    it 'places the correct answer in a different location every time' do
-      data_2 = {"id"=>9,
-      "topic"=>"Chess Trivia",
-      "question_text"=>"What is the most powerful piece in chess?",
-      "correct_answer"=>"Queen",
-      "options"=>["Queen", "King", "Knight", "Pawn"]}
-      @question_2 = Question.new(data_2)
-
+  describe "instance methods" do
+    it "#fix_question places the correct answer in a different location every time" do
       correct_index = @question.options.index("Pon de Replay")
       correct_index_2 = @question.options.index("Queen")
       
       expect(correct_index == correct_index_2).to be(false)
+    end
+
+    it "#complete_options adds the correct answer into the options in case the size of that array is less than 4" do
+    
+      expect(@question_3.options.length).to eq(4)
+      expect(@question_3.options.include?("Pon de Replay")).to eq(true)
     end
   end
 end
