248317bf86bb27601711f7d6e029f2c33aa2c049
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-05-18 17:37:03 -0500

    update question service again
    Co-authored-by: Laura R Vega <149700247+laurarvegav@users.noreply.github.com>

--- app/services/question_service.rb
+++ app/services/question_service.rb
@@ -1,42 +1,16 @@
-class QuestionService 
-  attr_reader :api_url, :options, :query
-
-  def initialize(number, topic)
-    @query = "Write #{number} trivia questions about #{topic}, and phrase the answers in two words or less. Return 1 correct answer and 3 incorrect answers. Format JSON with id and a key named topic associated to the topic, a key named question_text associated to the question, a key named correct_answer associated with the string of the correct answer and another key named options associated to an array of the 4 strings of the generated options"
-
-    @api_url = 'https://api.openai.com/v1/chat/completions'
-
-    @options = {
-      'Content-Type' => 'application/json',
-      'Authorization' => Rails.application.credentials.open_ai_key
-    }
+class QuestionService
+  def self.create_questions_for(game)
+    post_url("/api/v1/questions", game)
+    @parsed_json
   end
 
-  def call
-    body = {
-      model: 'gpt-3.5-turbo',
-      messages: [ { role: 'user', content: @query,
-      response_format: { "type": "json_object" } }]
-    }
-
-    response = connection.post(@api_url, body.to_json, @options)
-
-    unless response.success?
-      raise "Error: #{JSON.parse(response.body)['error']['message']}"
-    end
-
-    response.body
-
-    rescue StandardError => e
-      raise "Error calling OpenAI API: #{e.message}"
-    nil
+  def self.post_url(url, params)
+    response = conn.post(url, params)
+    @parsed_json = JSON.parse(response.body, symbolize_names: true)
   end
 
-  def connection
-    Faraday.new do |faraday|
-      faraday.request :json
-      faraday.response :json, content_type: /\bjson$/
-      faraday.adapter Faraday.default_adapter
-    end
+  def self.conn
+    # Faraday.new(url: "http://localhost:5000")
+    Faraday.new(url: "https://brain-defrost-be-questions.onrender.com")
   end
 end
