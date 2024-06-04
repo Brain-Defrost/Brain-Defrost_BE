49739104c5f6ed736c604e41e8741e00c43fcaac
Martin Chavez <martinchavez1203@gmail.com>
2024-06-03 22:05:19 -0600

    remove comments

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,11 +2,10 @@ require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
   test "invite" do
-    # Create the email and store it for further assertions
     email = UserMailer.create_invite("me@example.com",
                                      "friend@example.com", Time.now)
 
-    # Send the email, then test that it got queued
+  
    
   end
 end
