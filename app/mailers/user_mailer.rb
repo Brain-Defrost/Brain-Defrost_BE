0c0f90b4b305b14e1aa3de05ddab5d768a9ff9aa
Martin Chavez <martinchavez1203@gmail.com>
2024-06-04 11:13:24 -0600

    create invite method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,12 @@ class UserMailer < ApplicationMailer
     @stat = stat
     mail(to: email, subject: 'Your Statistics Report')
   end
+
+  def create_invite(sender_email, recipient_email, sent_at)
+    @sender_email = sender_email
+    @recipient_email = recipient_email
+    @sent_at = sent_at
+
+    mail(to: @recipient_email, from: @sender_email, subject: "You have been invited by #{@sender_email}")
+  end
 end
