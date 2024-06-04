f09b36ebf3f95357806a36a7832bc5abb2248971
Martin Chavez <martinchavez1203@gmail.com>
2024-06-04 10:56:32 -0600

    create test for UserMailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,3 +1,14 @@
 require "rails_helper"
 
+RSpec.describe UserMailer, type: :mailer do
+  describe 'invite' do
+    let(:mail) { UserMailer.create_invite('me@example.com', 'friend@example.com', Time.now) }
 
+    it 'renders the headers' do
+      expect(mail.subject).to eq('You have been invited by me@example.com')
+      expect(mail.to).to eq(['friend@example.com'])
+      expect(mail.from).to eq(['me@example.com'])
+    end
+
+  end
+end
