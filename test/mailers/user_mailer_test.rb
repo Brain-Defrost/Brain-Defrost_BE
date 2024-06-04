a84d806ee6cb92386e2f29fb66fea072aca8f5a5
Martin Chavez <martinchavez1203@gmail.com>
2024-06-03 22:10:35 -0600

    add more assertions to test block to be more robust

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -11,6 +11,8 @@ class UserMailerTest < ActionMailer::TestCase
 
     
     assert_equal ["me@example.com"], email.from
-    
+    assert_equal ["friend@example.com"], email.to
+    assert_equal "You have been invited by me@example.com", email.subject
+    assert_equal read_fixture("invite").join, email.body.to_s
   end
 end
