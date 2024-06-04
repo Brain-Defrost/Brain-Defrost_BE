e7a2217a17b3f50d40a95b6d11d6ee6e005da44c
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-06-04 11:27:29 -0500

    start websockets

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      self.current_player = find_verified_player
+    end
+
+    private
+
+    def find_verified_player
+      if verified_player = Player.find_by(game_id: params[:game_id], id: params[:id])
+        verified_player
+      else
+        # built-in -> closes websocket connection if open and returns “unauthorized” reason. 
+        reject_unauthorized_connection
+      end
+    end
   end
 end
---
+++ app/channels/game_channel.rb
@@ -0,0 +1,11 @@
+class GameChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    game = Game.find(params[:game_id])
+    stream_for game
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
