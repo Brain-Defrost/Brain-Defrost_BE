6979a3d367ba42bd5e50274b91f4012712bb5302
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-06-07 03:11:36 -0600

    Fix to use sidekiq test mode and adjust expected value

--- spec/jobs/stat_sender_job_spec.rb
+++ spec/jobs/stat_sender_job_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe StatSenderJob, type: :job do
+  before do
+    Sidekiq::Testing.fake!
+  end
+
   let(:game) { create(:game) }
   let(:stat_id) { create(:stat, game: game).id }
   let(:email) { 'test@example.com' }
-
-  it 'sends an email' do
+  
+  it 'queues the job' do
     expect {
       described_class.perform_async(email, stat_id)
-    }.to change{ Sidekiq::Queue.new('mailers').size }.by(1)
+    }.to change { Sidekiq::Queues['default'].size }.by(1)
   end
 end
