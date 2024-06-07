45cf93259cdd872d77212d0a1fbba9fbceb2d3f6
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-06-07 00:43:53 -0500

    game player_list broadcasts

--- spec/requests/api/v1/broadcasts_spec.rb
+++ spec/requests/api/v1/broadcasts_spec.rb
@@ -48,4 +48,42 @@ RSpec.describe "ActionCable Broadcasts", type: :request do
       )
     end
   end
+
+  describe "Game's player list" do
+    it "GET /api/v1/games/:id/players broadcast" do
+      game = create(:game)
+      create_list(:player, 2, game_id: game.id)
+      players = game.players.as_json(except: [:created_at, :updated_at])
+
+      get "/api/v1/games/#{game.id}/players"
+
+      expect {
+        ActionCable.server.broadcast("game_channel_#{game.id}", { player_list: players })
+      }.to have_broadcasted_to("game_channel_#{game.id}").with(player_list: players)
+    end
+
+    it "POST /api/v1/games/:id/players broadcast" do
+      game = create(:game)
+
+      post "/api/v1/games/#{game.id}/players", params: { display_name: "person"}
+
+      players = game.players.as_json(except: [:created_at, :updated_at])
+
+      expect {
+        ActionCable.server.broadcast("game_channel_#{game.id}", { player_list: players })
+      }.to have_broadcasted_to("game_channel_#{game.id}").with(player_list: players)
+    end
+
+    it "PATCH /api/v1/games/:game_id/players/:id broadcast" do
+      game = create(:game)
+      player = create(:player, game_id: game.id)
+      players = game.players.as_json(except: [:created_at, :updated_at])
+
+      patch "/api/v1/games/#{game.id}", params: { started: true}
+
+      expect {
+        ActionCable.server.broadcast("game_channel_#{game.id}", { player_list: players })
+      }.to have_broadcasted_to("game_channel_#{game.id}").with(player_list: players)
+    end
+  end
 end
