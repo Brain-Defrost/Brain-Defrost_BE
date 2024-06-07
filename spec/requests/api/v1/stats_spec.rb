5a65485cd62d8b6e54580f10e799bbd9281ef3dd
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-06-07 02:27:01 -0500

    update get stats test

--- spec/requests/api/v1/stats_spec.rb
+++ spec/requests/api/v1/stats_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'Stats API', type: :request do
 
       response(200, 'successful') do
         let(:game_id) { create(:game, number_of_questions: 2).id }
-        let(:id) { create(:player, answers_correct: 2, game_id: game_id) }
         before { create(:player, answers_correct: 0, game_id: game_id) }
         before { create(:player, answers_correct: 1, game_id: game_id) }
 
@@ -95,31 +94,23 @@ RSpec.describe 'Stats API', type: :request do
 
         run_test! do |example|
           expect(response).to have_http_status(200)
-          parsed_data = JSON.parse(response.body, symbolize_names: true)[:data]
 
-          expect(parsed_data).to be_a(Hash)
-          expect(parsed_data[:id]).to be_a(Integer)
-          expect(parsed_data[:type]).to be_a(String)
+          parsed_data = JSON.parse(response.body, symbolize_names: true)[:data]
+          expect(parsed_data[:id].to_i).to eq Stat.last.id
+          expect(parsed_data[:type]).to eq "stat"
 
           attributes = parsed_data[:attributes]
-
-          expect(attributes[:game_id]).to be_a(Integer)
-          expect(attributes[:avg_correct_answers]).to be_a(Float)
+          expect(attributes[:game_id]).to eq game_id
           expect(attributes[:avg_correct_answers]).to eq 50.0
 
-          relationships = parsed_data[:relationships]
+          games = parsed_data[:relationships][:games][:data]
+          expect(games.size).to eq 1
+          expect(games.first[:id]).to eq game_id
+          expect(games.first[:type]).to eq "game"
 
-          expect(relationships).to be_a(Hash)
-
-          expect( relationships[:games][:data]).to be_a(Array)
-          expect( relationships[:games][:data].first[:id]).to be_a(Integer)
-          expect( relationships[:games][:data].first[:type]).to be_a(String)
-          expect( relationships[:games][:data].first[:attributes]).to be_a(Hash)
-          expect( relationships[:games][:data].first[:attributes][:link]).to be_a(String)
-          expect( relationships[:games][:data].first[:attributes][:number_of_questions]).to be_a(Integer)
-          expect( relationships[:games][:data].first[:attributes][:number_of_players]).to be_a(Integer)
-          expect( relationships[:games][:data].first[:attributes][:topic]).to be_a(String)
-          expect( relationships[:games][:data].first[:attributes][:time_limit]).to be_a(Integer)
+          players = parsed_data[:relationships][:players][:data]
+          expect(players.size).to eq 2
+          expect(players).to all(include(type: "player"))
         end
       end
 
@@ -136,15 +127,20 @@ RSpec.describe 'Stats API', type: :request do
     end
   end
 
-  path '/api/v1/games/{game_id}/stats/{email}' do
+  path '/api/v1/games/{game_id}/stats/email' do
     parameter name: :game_id, in: :path, type: :string, description: 'Game ID'
-    parameter name: :email, in: :path, type: :string, description: 'Player Email'
 
     post("Send game's stats via Email") do
       tags 'Stats'
+      consumes 'application/json'
       produces 'application/json'
 
-      response(200, 'successful') do
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: { email: { type: :string, required: true } }
+      }
+
+      response(200, 'successfully created') do
         let(:game_id) { create(:game, number_of_questions: 2).id }
         let(:email) { 'example@mail.com' }
         before { 2.times { create(:player, answers_correct: 1, game_id: game_id) } }
