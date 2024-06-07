98bf760beba79650405815eafa7252194ce35be7
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-06-06 21:24:03 -0500

    complete game channel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,11 +1,11 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # require game_id
     return reject unless params[:game_id].present?
 
-    # needs to be find_by and not find
     if game = Game.find_by(id: params[:game_id])
       stream_for game
+      players = game.players.as_json(except: [:created_at, :updated_at])
+      GameChannel.broadcast_to(game, { player_list: players, game_started: game.started })
     else
       reject
     end
